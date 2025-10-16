f30a03fa2ae341867af775b23a2a0d10e681928f
halimbagusalhakim <halsat132@gmail.com>
2025-10-16 19:32:02 +0700

    lanjuta membuat fungsi tataletakbboxcolumnrow menambahkan style

--- app/src/main/java/com/example/tataletak/tataletak.kt
+++ app/src/main/java/com/example/tataletak/tataletak.kt
@@ -159,6 +159,27 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
             }
         }
         Spacer(modifier = Modifier.height(10.dp))
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit
+            )
+            Text(
+                text = "My Music",
+                fontSize = 50.sp,
+                color = Color.Red
+            )
+        }
+    }
+}
+
 
 
 
